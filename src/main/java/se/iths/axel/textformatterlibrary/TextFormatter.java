c437b111adc6a138e1facf7120daae14f413bba9
Axsty <axelstaaffriborg@gmail.com>
2026-03-31 10:38:01 +0200

    Inital commit

--- src/main/java/se/iths/axel/textformatterlibrary/TextFormatter.java
+++ src/main/java/se/iths/axel/textformatterlibrary/TextFormatter.java
@@ -1,4 +1,19 @@
-package se.iths.axel;
+package se.iths.axel.textformatterlibrary;
 
 public class TextFormatter {
+
+    public String toUpperCase(String string) {
+        String formatted = string.toUpperCase();
+        return formatted;
+    }
+
+    public String toLowerCase(String string) {
+        String formatted = string.toLowerCase();
+        return formatted;
+    }
+
+    public String trimString(String string) {
+        String formatted = string.trim();
+        return formatted;
+    }
 }
--- src/main/java/se/iths/axel/textformatterlibrary/TextFormatterConfig.java
+++ src/main/java/se/iths/axel/textformatterlibrary/TextFormatterConfig.java
@@ -1,4 +1,13 @@
 package se.iths.axel.textformatterlibrary;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class TextFormatterConfig {
+
+    @Bean
+    public TextFormatter textFormatter() {
+        return new TextFormatter();
+    }
 }
